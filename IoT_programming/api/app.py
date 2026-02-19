321d47959a5f4565395837070b07d3908cff9a20
J00NQ <kjk020913@gmail.com>
2026-02-19 12:34:23 +0900

    update app.py

--- IoT_programming/api/app.py
+++ IoT_programming/api/app.py
@@ -91,3 +91,21 @@ def timeline(user_id):
         'user_id' : user_id,
         'timeline' : timeline
     })
+
+def create_app(test_config = None):
+    app = Flask(__name__)
+
+    app.json_encoder = CustomJSONProvider
+
+    if test_config is None:
+        app.config.from_pyfile("config.py")
+    else:
+        app.config.update(test_config)
+
+    database     = create_engine(app.config['DB_URL'], encoding = 'utf-8', max_overflow = 0)
+    app.database = database 
+
+    return app
+
+if __name__ == "__main__":
+    app.run(host = "127.0.0.1", port=5000)
